9df503174beed7489bf9ccee71ebbe2f6bfce471
Zhudl2003 <119029839+Zhudl2003@users.noreply.github.com>
2022-11-30 22:42:41 +0300

    Add files via upload

--- 1.R
+++ 1.R
@@ -6,10 +6,10 @@ library(tidyverse)
 
 aa_table_1<-read_csv("charmm-gui-3136192901.csv")
 int_1<-read_csv("charmm-gui-3136192901 (1).csv")
-filter(int_1,persent_ring>0)
+int_1<-filter(int_1,persent_ring>0)
 aa_table_2<-read_csv("charmm-gui-3136217149.csv")
 int_2<-read_csv("charmm-gui-3136217149 (1).csv")
-filter(int_2,persent_ring>0)
+int_2<-filter(int_2,persent_ring>0)
 
 nodes<-aa_table_1 %>%
   rename(id=resno) %>%
@@ -20,7 +20,7 @@ head(nodes, 3)
 edges<-int_1 %>%
   rename(weight = persent_ring)
 aa_net <- tbl_graph(
-  nodes = nodes, edges = edges, directed = TRUE)
+  nodes = nodes, edges = edges, directed = FALSE)
 summary(aa_net)
 
 nodes_1<-aa_table_2 %>%
@@ -32,6 +32,6 @@ head(nodes_1, 3)
 edges_1<-int_2 %>%
   rename(weight = persent_ring)
 aa_net_1 <- tbl_graph(
-  nodes = nodes, edges = edges, directed = TRUE)
+  nodes = nodes, edges = edges, directed = FALSE)
 summary(aa_net_1)
 
